4e72e468953b73d33213d7a8ef140e9f6a49e1b5
Endy Muhardin <endy.muhardin@gmail.com>
2016-11-15 09:30:49 +0700

    starter project

---
+++ aplikasi-backend/src/main/java/com/muhardin/endy/belajar/jpos/AplikasiBackendApplication.java
@@ -0,0 +1,12 @@
+package com.muhardin.endy.belajar.jpos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AplikasiBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AplikasiBackendApplication.class, args);
+	}
+}
---
+++ aplikasi-backend/src/test/java/com/muhardin/endy/belajar/jpos/AplikasiBackendApplicationTests.java
@@ -0,0 +1,16 @@
+package com.muhardin.endy.belajar.jpos;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class AplikasiBackendApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
